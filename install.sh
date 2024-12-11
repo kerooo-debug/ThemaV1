c74be785e145f2144fdd7c480d5c41838ce691ac
kerooo-debug <144114369+kerooo-debug@users.noreply.github.com>
2024-12-11 09:43:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,7 +92,7 @@ install_theme() {
     read -r SELECT_THEME
     case "$SELECT_THEME" in
       1)
-        THEME_URL=$(echo -e "https://github.com/kerooo-debug/Thema/raw/main/C2.zip")
+        THEME_URL=$(echo -e "https://github.com/kerooo-debug/ThemaV1/raw/master/C2.zip")
         break
         ;;
       2)
@@ -100,7 +100,7 @@ install_theme() {
         break
         ;;
       3)
-        THEME_URL=$(echo -e "https://github.com/kerooo-debug/Thema/raw/main/C3.zip")
+        THEME_URL=$(echo -e "https://github.com/kerooo-debug/ThemaV1/raw/master/C3.zip")
         break
         ;; 
       x)
